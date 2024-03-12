4e945a32f043da87fbadda99a377fb08e1663904
Shivam Rajput <rshivam973@gmail.com>
2024-03-12 14:17:59 +0530

    1679

--- Max_no_of_ksum_pairs.js
+++ Max_no_of_ksum_pairs.js
@@ -1,17 +1,14 @@
 var maxOperations = function(nums, k) {
-    let i=0,j=nums.length-1,count=0;
-    nums.sort();
-    while(i<j){
-        let sum = nums[i]+nums[j];
-        if(sum===k){
+    let i=0,j=0,count=0;
+    let n = nums.length;
+    const mp = new Map();
+    for(i=0;i<n;i++){
+        if(mp.get(k-nums[i])){
             count++;
-            i++;
-            j--;
-        }
-        else if(sum<k){
-            i++;
+            if(mp.get(k-nums[i])==1) mp.delete(k-nums[i]);
+            else mp.set(k-nums[i],mp.get(k-nums[i])-1);
         }else{
-            j--;
+            mp.set(nums[i],mp.get(nums[i])+1 || 1);
         }
     }
     return count;
